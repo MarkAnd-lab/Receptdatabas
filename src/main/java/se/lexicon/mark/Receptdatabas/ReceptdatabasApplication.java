08432ddc763e4f9c80afdff59c80db63cb6852c6
MarkAnd-lab <mark-andersson@hotmail.com>
2020-06-02 15:49:09 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/mark/Receptdatabas/ReceptdatabasApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.mark.Receptdatabas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptdatabasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptdatabasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/mark/Receptdatabas/ReceptdatabasApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mark.Receptdatabas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptdatabasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
