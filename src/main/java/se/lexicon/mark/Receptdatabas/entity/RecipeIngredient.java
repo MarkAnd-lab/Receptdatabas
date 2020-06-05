1876deaac704749c2b404e9c68bab1b366e7ba11
MarkAnd-lab <mark-andersson@hotmail.com>
2020-06-05 10:37:13 +0200

    RecipeCategory

--- src/main/java/se/lexicon/mark/Receptdatabas/entity/RecipeIngredient.java
+++ src/main/java/se/lexicon/mark/Receptdatabas/entity/RecipeIngredient.java
@@ -3,104 +3,31 @@ package se.lexicon.mark.Receptdatabas.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
-public class RecipeIngredient<Recipe> {
+public class RecipeIngredient {
+
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+)
     @Column(name = "recipeIngredientId", updatable = false, nullable = false)
     private String recipeIngredientId;
-    Ingredient ingredient;
-    private double amount;
-    Measurement measurement;
-    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},
+
+    @ManyToOne (cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},
             fetch = FetchType.EAGER)
-    Recipe recipe;
+    @JoinColumn(name = "ingredientId")
+    private Ingredient ingredient;
 
+    private double amount;
+    private Measurement measurement;
+    @ManyToOne (cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},
+            fetch = FetchType.EAGER)
+    @JoinColumn(name = "recipeId")
+    private Recipe recipe;
+    public RecipeIngredient(){
 
-    public RecipeIngredient(Ingredient ingredient, double amount, Measurement measurement, Recipe recipe) {
-        this.ingredient = ingredient;
-        this.amount = amount;
-        this.measurement = measurement;
-        this.recipe = recipe;
-    }
-
-    public RecipeIngredient(String recipeIngredientId, Ingredient ingredient, double amount, Measurement measurement, Recipe recipe) {
-        this.recipeIngredientId = recipeIngredientId;
-        this.ingredient = ingredient;
-        this.amount = amount;
-        this.measurement = measurement;
-        this.recipe = recipe;
-
-
-    }
-
-    public String getRecipeIngredientId() {
-        return recipeIngredientId;
-    }
-
-    public Ingredient getIngredient() {
-        return ingredient;
-    }
-
-    public void setIngredient(Ingredient ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public Measurement getMeasurement() {
-        return measurement;
-    }
-
-    public void setMeasurement(Measurement measurement) {
-        this.measurement = measurement;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RecipeIngredient<?> that = (RecipeIngredient<?>) o;
-        return Double.compare(that.amount, amount) == 0 &&
-                Objects.equals(recipeIngredientId, that.recipeIngredientId) &&
-                Objects.equals(ingredient, that.ingredient) &&
-                measurement == that.measurement &&
-                Objects.equals(recipe, that.recipe);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(recipeIngredientId, ingredient, amount, measurement, recipe);
-    }
-
-    @Override
-    public String toString() {
-        return "RecipeIngredient{" +
-                "recipeIngredientId='" + recipeIngredientId + '\'' +
-                ", ingredient=" + ingredient +
-                ", amount=" + amount +
-                ", measurement=" + measurement +
-                ", recipe=" + recipe +
-                '}';
     }
 }
